b0509106cd548f2c1a65beebe2e5e16f6bf6d05b
boonloong <obl0702@gmail.com>
2022-11-04 14:53:26 +0800

    Adding DML & Function name in Repo

--- src/main/java/com/ongbl/controllers/IndexController.java
+++ src/main/java/com/ongbl/controllers/IndexController.java
@@ -1,7 +1,14 @@
 package com.ongbl.controllers;
 
+import com.ongbl.domain.Category;
+import com.ongbl.domain.UnitOfMeasure;
+import com.ongbl.repositories.CategoryRepository;
+import com.ongbl.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 /**
  * @created: 4/11/2022
@@ -10,8 +17,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("categoryOptional: " + categoryOptional.get().getId());
+        System.out.println("unitOfMeasureOptional: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/com/ongbl/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/ongbl/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.ongbl.repositories;
 import com.ongbl.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
