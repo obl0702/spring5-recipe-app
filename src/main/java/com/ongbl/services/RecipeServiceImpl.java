61938e8a2395de90949e39534a7e0218e050240e
boonloong <obl0702@gmail.com>
2022-11-05 11:53:07 +0800

    Added Logging to RecipeServiceImpl by Lombok Slf4j. Closes #2

--- src/main/java/com/ongbl/services/RecipeServiceImpl.java
+++ src/main/java/com/ongbl/services/RecipeServiceImpl.java
@@ -2,11 +2,12 @@ package com.ongbl.services;
 
 import com.ongbl.domain.Recipe;
 import com.ongbl.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
