5385e451a8e6b1bba7ea1736dea3a4e7c03ce8a9
boonloong <obl0702@gmail.com>
2022-11-08 14:25:38 +0800

    Created a Command Object and Type Conversion. Closes #16

---
+++ src/main/java/com/ongbl/commands/NotesCommand.java
@@ -0,0 +1,14 @@
+package com.ongbl.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NotesCommand {
+    private Long id;
+    private String recipeNotes;
+
+}
