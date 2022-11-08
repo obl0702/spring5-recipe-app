84c2d4b87251399c63f4e67859132650713f0828
boonloong <obl0702@gmail.com>
2022-11-08 23:53:32 +0800

    Showed Bad Request 400 for Number Format Exception . Closes #20

--- src/main/java/com/ongbl/controllers/RecipeController.java
+++ src/main/java/com/ongbl/controllers/RecipeController.java
@@ -69,4 +69,18 @@ public class RecipeController {
 
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberException(Exception ex){
+
+        log.error("Handling Number Format exception");
+        log.error(ex.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", ex);
+
+        return modelAndView;
+    }
 }
