3b1a490ae2c1fc634cdd83acee8c8e5f20c1a465
boonloong <obl0702@gmail.com>
2022-11-08 14:38:49 +0800

    Added Type Conversion. Closes #16

--- src/main/java/com/ongbl/services/RecipeService.java
+++ src/main/java/com/ongbl/services/RecipeService.java
@@ -1,8 +1,17 @@
 package com.ongbl.services;
 
+import com.ongbl.commands.RecipeCommand;
 import com.ongbl.domain.Recipe;
 import java.util.Set;
 
 public interface RecipeService {
     Set<Recipe> getRecipes();
+
+    Recipe findById(Long l);
+
+    RecipeCommand findCommandById(Long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+
+    void deleteById(Long idToDelete);
 }
