20715c4ea12a8345a1f12157009b63070f9c5a20
boonloong <obl0702@gmail.com>
2022-11-09 19:01:35 +0800

    Display Validation Errors on the Thymeleaf. Closes #23

--- src/main/java/com/ongbl/controllers/RecipeController.java
+++ src/main/java/com/ongbl/controllers/RecipeController.java
@@ -45,7 +45,7 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@Valid @ModelAttribute RecipeCommand command,
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command,
             BindingResult bindingResult){
 
         if(bindingResult.hasErrors()){
--- src/main/java/com/ongbl/domain/Difficulty.java
+++ src/main/java/com/ongbl/domain/Difficulty.java
@@ -1,5 +1,5 @@
 package com.ongbl.domain;
 
 public enum Difficulty {
-    EASY, MODERATE, HARD
+    EASY, MODERATE, KIND_OF_HARD, HARD
 }
